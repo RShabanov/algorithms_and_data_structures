922b5ee65cbc2a8952bbcdacf29f73514ac28ee8
Shabanov Roman <shabanov.roman0@gmail.com>
2021-03-14 14:55:50 +0800

    Fix some errors

--- hash/my_hash.cpp
+++ hash/my_hash.cpp
@@ -1,34 +1,45 @@
 #include <iostream>
+#include <functional>
 #include <fstream>
 #include <string>
 #include <set>
 using namespace std;
 
 
-int get_collision_amount(char*, unsigned long long(*)(const string&));
+int get_collision_amount(char*, function<unsigned long long(const string&)>, size_t);
 unsigned long long poly_hash(const string&);
 unsigned long long ASCII_hash(const string&);
 unsigned long long quick_pow(int, unsigned int);
 
 
 int main(int argc, char* argv[]) {
-    if (argc > 1)      
+    if (argc > 1) {
+        size_t hash_size = 0;
+        cout << "Enter hash size: ";
+        while (!(cin >> hash_size)) {
+            cin.ignore(numeric_limits<streamsize>::max(),'\n');
+            cout << "Bad input - enter hash size again: ";
+        }
+        
         for (int i = 1; i < argc; i++) {
             cout << "File: " << argv[i] << endl;
             cout << "Collision amount (" << argv[i] << ") using poly_hash:\t" 
-                << get_collision_amount(argv[i], poly_hash) << endl;
+                << get_collision_amount(argv[i], poly_hash, hash_size) << endl;
                 
             cout << "Collision amount (" << argv[i] << ") using ASCII_hash:\t" 
-                << get_collision_amount(argv[i], ASCII_hash) << endl << endl;
+                << get_collision_amount(argv[i], ASCII_hash, hash_size) << endl << endl;
         }
+    }
     
-    else cerr << "Error! There mush be a file path" << endl;
+    else cerr << "Error! There must be a file path" << endl;
     
     return 0;
 }
 
 
-int get_collision_amount(char* filepath, unsigned long long(*fun_hash)(const string&)) {
+int get_collision_amount(char* filepath, 
+                         function<unsigned long long(const string&)> fun_hash, 
+                         size_t hash_size) {
     ifstream file(filepath);
     if (!file.is_open()) {
         cerr << "File cannot be opened\n";
@@ -39,7 +50,7 @@ int get_collision_amount(char* filepath, unsigned long long(*fun_hash)(const str
     set<unsigned long long> keys_set;
     string key;
     while (file >> key)
-        collisions += keys_set.emplace(fun_hash(key)).second;
+        collisions += !keys_set.emplace(fun_hash(key) % hash_size).second;
     
     return collisions;
 }
